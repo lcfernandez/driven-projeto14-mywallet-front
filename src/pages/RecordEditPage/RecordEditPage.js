3ed3d7d80c1dffbbb70f6d0001e9dc4615cffee6
lcfernandez <lcfernandez@cos.ufrj.br>
2022-11-20 01:20:48 -0300

    feat: implement edit record page design

--- src/pages/RecordEditPage/RecordEditPage.js
+++ src/pages/RecordEditPage/RecordEditPage.js
@@ -1,5 +1,41 @@
+import FormStyle from "../../assets/styles/FormStyle";
+import Header from "../../components/Header";
+
+import styled from "styled-components";
+
 export default function RecordEditPage() {
+    function editRecord(e) {
+        e.preventDefault(); // prevent form redirect
+    }
+
     return (
-        <></>
+        <RecordEditPageContainer onSubmit={editRecord}>
+            <Header text="Editar entrada/saída" />
+
+            <FormStyle>
+                <input
+                    min="0.01"
+                    onWheel={e => e.target.blur()}
+                    placeholder="Valor"
+                    required
+                    step="0.01"
+                    type="number"
+                />
+
+                <input
+                    placeholder="Descrição"
+                    required
+                    type="text"
+                />
+
+                <button>
+                    Atualizar entrada/saída
+                </button>
+            </FormStyle>
+        </RecordEditPageContainer>
     );
 }
+
+const RecordEditPageContainer = styled.div`
+    padding: 0 25px;
+`;
